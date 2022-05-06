5e30b2d25e7e3a995acfc0fec2045b597a512459
jambrose0 <jambrose0518@gmail.com>
2022-05-06 13:57:24 -0400

    added pizza routes and controller functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,19 +1,23 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
   pizzaName: {
-    type: String,
+    type: String
   },
   createdBy: {
-    type: String,
+    type: String
   },
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
   size: {
     type: String,
-    default: "Large",
+    default: 'Large'
   },
-  toppings: [],
+  toppings: []
 });
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
